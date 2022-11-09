bcfb4685e0937d20266815e8b3532532e5663fe1
liamh <d00236258@student.dkit.ie>
2022-11-09 10:27:45 +0000

    Added Login/Register Page

--- application.py
+++ application.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
-import json
+from flask import Flask, request, json, session
+from flask import render_template
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
+app.debug = True
 
 alive = 0
 data = {}
@@ -15,9 +17,30 @@ def keep_alive():
     global alive, data
     alive +=1
     keep_alive_count = str(alive)
-    dta['keep_alive'] = keep_alie_count
+    data['keep_alive'] = keep_alive_count
     parsed_json = json.dumps(data)
     print(parsed_json)
     return str(parsed_json)
 
 app.run( port = 5000)
+
+@app.route("/signin", methods=['GET','POST'])
+def signin():
+    #username = request.form['username']
+    #password = request.form['password']
+    #if username and password:
+       # return json.dumps({'validation': validateUser(username, password)})
+    #return json.dumps({'validation': False})
+    return render_template('signin.html')
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+    return render_template('register.html')
+
+
+def validateUser(username, password, confirmPassword):
+    return True
+
+
+if __name__ == '__main__':
+    app.run()
